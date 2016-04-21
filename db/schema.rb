57f3f0de02610c815f5a67b92e7d87d85e53d0b2
shouta_fukuda <fukuda.shouta.kakaku@gmail.com>
2016-04-21 08:12:48 +0000

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421024547) do
+ActiveRecord::Schema.define(version: 20160421050654) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 20160421024547) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "nickname"
+    t.string   "place"
+    t.string   "description"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

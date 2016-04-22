dd2ab001bb266ac68eee8d346959d0d7e7e395a2
shouta_fukuda <fukuda.shouta.kakaku@gmail.com>
2016-04-22 00:23:44 +0000

    Handle no record exception with 404.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
   private
   def logged_in_user
@@ -9,5 +10,9 @@ class ApplicationController < ActionController::Base
       flash[:danger] = "Please log in."
       redirect_to login_url
     end
+  end
+
+  def render_404
+    render text: "404", status: 404
   end
 end

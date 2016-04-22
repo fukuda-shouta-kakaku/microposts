aca40cf0af9844e9ba5d4ff1a4c1248cb6234013
shouta_fukuda <fukuda.shouta.kakaku@gmail.com>
2016-04-22 01:12:27 +0000

    Authenticate user in logged_in_user method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
   private
   def logged_in_user
-    unless logged_in?
+    @user = User.find_by_id(params[:id])
+    unless logged_in? && @user == current_user
       store_location
       flash[:danger] = "Please log in."
       redirect_to login_url
     end
-  end
-
-  def render_404
-    render text: "404", status: 404
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
-  before_action :authenticate_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:show, :edit, :update]
 
   def show
   end
@@ -36,21 +35,9 @@ class UsersController < ApplicationController
   end
 
   private
-  def set_user
-    @user = User.find(params[:id])
-  end
-
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :nickname, :place, :description,
                                  :password_confirmation)
-  end
-
-  def authenticate_user
-    unless session[:user_id] == params[:id].to_i
-      flash[:danger] = "Invalid user."
-      redirect_to root_path
-    end
-
   end
 end
